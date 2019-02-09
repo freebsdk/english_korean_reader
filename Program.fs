e2561bbf9be88a7b0482c7aeea00a5c942efc99b
SeokYoung Shin <aceorb@gmail.com>
2019-02-10 00:19:45 +0900

    Remove test code.

--- Program.fs
+++ Program.fs
@@ -10,12 +10,4 @@ open EnglishKoreanReader
 
 [<EntryPoint>]
 let main argv =
-    let hs = HangulCodeTable.HangulString()
-    hs.AddSyllable('ㄱ')
-    hs.AddSyllable('ㅏ')
-    hs.AddSyllable('ㄴ')
-    hs.AddSyllable('ㄷ')
-    hs.AddSyllable('ㅏ')
-    hs.AddSyllable('ㄴ')
-    printfn "%s" (hs.ToString())
     0 // return an integer exit code
